46384c997ae3f8ecc90b2c0c81ec652a47d39d4a
JosteinE <Josteineri@hotmail.com>
2015-08-10 23:24:15 +0200

    Laget en bakke og spiller med A og D kontroller

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	float Speed = 5;
+
+	void Update ()
+	{
+		Movement();
+	}
+
+	void Movement ()
+	{
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.Translate(Vector2.right * Speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.Translate(-Vector2.right * Speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Space))
+		{
+			transform.Translate(Vector2.up * Speed * Time.deltaTime);
+		}
+	}
+}
