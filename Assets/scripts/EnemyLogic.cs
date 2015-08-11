f809d30ac345fdddd3d9cbeeda077a5fb19e4c40
JosteinE <Josteineri@hotmail.com>
2015-08-11 03:20:38 +0200

    Lagde en fiende og ga spilleren muligheten til å hoppe (lavt).

---
+++ Assets/scripts/EnemyLogic.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLogic : MonoBehaviour {
+
+	public Transform sightStart, sightEnd;
+
+	public bool spotted = false;
+	public bool facingLeft = true;
+
+	public GameObject spottedCube;
+
+	void Start()
+	{
+		InvokeRepeating("Patrol", 0f, Random.Range (2f,6f));
+	}
+	void Update ()
+	{
+		Raycasting();
+		Behaviour();
+	}
+
+	void Raycasting()
+	{
+		//draws the line
+		Debug.DrawLine (sightStart.position, sightEnd.position, Color.red);
+		//detects the player
+		spotted = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("Player"));
+	}
+
+	void Behaviour()
+	{
+		if (spotted == true) {
+			spottedCube.SetActive (true);
+		}
+		else
+		{
+			spottedCube.SetActive (false);
+		}
+	}
+
+	void Patrol()
+	{
+		//Flip left and right at random intervals
+		facingLeft = !facingLeft;
+
+		if(facingLeft == true)
+		{
+			transform.eulerAngles = new Vector2 (0, 0);
+		}
+		else
+		{
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+
+	}
+}
--- Assets/scripts/PlayerControl.cs
+++ Assets/scripts/PlayerControl.cs
@@ -3,28 +3,47 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+	//player
 	public float Speed = 7;
-	public float JumpSpeed = 20;
-	public float Gravity = 9.8f;
-
+	public float JumpSpeed = 20f;
+	public bool grounded;
+	public Transform JumpLineEnd;
+	
 	void Update ()
 	{
 		Movement();
+		Raycasting ();
 	}
 
-	void Movement ()
+	void Raycasting()
+	{
+		//draws the line
+		Debug.DrawLine (this.transform.position, JumpLineEnd.position, Color.red);
+
+		grounded = Physics2D.Linecast (this.transform.position, JumpLineEnd.position, 1 << LayerMask.NameToLayer ("Ground"));
+
+		Physics2D.IgnoreLayerCollision (8, 10);
+	}
+
+	void Movement()
 	{
 		if (Input.GetKey (KeyCode.D))
 		{
-			transform.Translate(Vector2.right * Speed * Time.deltaTime);
+			//moves the player to the right
+			transform.Translate (Vector2.right * Speed * Time.deltaTime);
+			//Flips the sprite, so that it looks to the right
+			transform.eulerAngles = new Vector2(0, 0);
 		}
 		if (Input.GetKey (KeyCode.A))
 		{
-			transform.Translate(-Vector2.right * Speed * Time.deltaTime);
+			// moves the player to the left, because the vector also gets flipped
+			transform.Translate (Vector2.right * Speed * Time.deltaTime);
+			//flips the vector and sprite
+			transform.eulerAngles = new Vector2(0, 180);
 		}
-		if (Input.GetKey (KeyCode.Space))
+		if (Input.GetKeyDown (KeyCode.Space) && grounded == true)
 		{
-			transform.Translate(Vector2.up * Speed * Time.deltaTime);
+			transform.Translate (Vector2.up * JumpSpeed * Time.deltaTime);
 		}
 	}
 }
