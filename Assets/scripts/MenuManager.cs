cb374cfb051130269783be35ca4c28dc32237e53
jorgeer <jfemail3@gmail.com>
2015-08-11 22:43:20 +0200

    Full meny med highscore, exit og start game

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public Menu CurrentMenu;
+
+	public void Start() {
+		ShowMenu(CurrentMenu);
+	}
+
+	public void ShowMenu (Menu menu) {
+		if (CurrentMenu != null)
+			CurrentMenu.IsOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;	
+	}
+
+	public void NewGame () {
+		// Should have separate panel for this
+		// that handles player creation, save creation and transition
+		// to game
+		Application.LoadLevel(1);
+	}
+
+	public void ExitGame () {
+		Application.Quit();
+	}
+
+
+}
--- Assets/scripts/menu.cs
+++ Assets/scripts/menu.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class menu : MonoBehaviour {
+public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool IsOpen {
+		get { return _animator.GetBool("IsOpen"); }
+		set { _animator.SetBool("IsOpen", value); } 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Awake () {
+		_animator = GetComponent<Animator>();
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+	}
+
+	public void Update() {
+		if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open")) {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+		} else {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+		}
+
 	}
 }
