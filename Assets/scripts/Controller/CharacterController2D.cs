3ab533c773ba2207499f0a9c7a117170a70f5b2f
JosteinE <Josteineri@hotmail.com>
2015-08-12 23:06:02 +0200

    Jobber med en nye CharacterController script

---
+++ Assets/scripts/Controller/CharacterController2D.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2D : MonoBehaviour
+{
+	private const float SkinWidth = .02f;
+	private const int TotalHorizontalRays = 8;
+	private const int TotalVerticalRays = 4;
+
+	private static readonly float SlopeLimitTangant = Mathf.Tan(75f * Mathf.Deg2Rad);
+
+	public LayerMask PlatformMask;
+	public ControllerParameter2D DefaultParameters;
+
+	public ControllerState2D state { get; private set; }
+	public Vector2 Velocity { get; private set; }
+	public bool CanJump { get { return false; } }
+
+	public void Awake() 
+	{
+		state = new ControllerState2D ();
+	}
+
+	public void AddForce(Vector2 force)
+	{
+
+	}
+
+	public void SetForce(Vector2 force)
+	{
+
+	}
+
+	public void SetHorizontalForce(float x)
+	{
+
+	}
+
+	public void SetVerticalForce(float y)
+	{
+
+	}
+
+	public void Jump()
+	{
+
+	}
+
+	public void LateUpdate()
+	{
+
+	}
+
+	private void Move(Vector2 deltaMovement)
+	{
+
+	}
+
+	private void HandlePlatforms()
+	{
+
+	}
+
+	private void CalculateRayOrigins()
+	{
+
+	}
+
+	private void MoveHorizontally(ref Vector2 deltaMovement)
+	{
+
+	}
+
+	private void MoveVertically(ref Vector2 deltaMovement)
+	{
+
+	}
+
+	private void HandleVerticalSlope(ref Vector2 deltaMovement)
+	{
+
+	}
+
+	private void HandleHorizontalSlope(ref Vector2 deltaMovement, float angle, bool isGoingRight)
+	{
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+
+	}
+
+	public void OnTriggerExit2D(Collider2D other)
+	{
+
+	}
+}
---
+++ Assets/scripts/Controller/ControllerParameter2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class ControllerParameter2D
+{
+	public enum JumpBehavior
+	{
+		CanJumpOnGround,
+		CanJumpAnywhere,
+		CantJump
+	}
+
+	public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
+
+	[Range(0, 90)]
+	public float SlopeLimit = 30;
+
+	public float Gravity = -25f;
+
+	public JumpBehavior JumpRestrictions;
+
+	public float JumpFrequency = .25f;
+}
